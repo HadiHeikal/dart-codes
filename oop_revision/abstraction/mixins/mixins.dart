0e8cf5900214dfa7d6f02c69c05364f1b96d6c1d
HadiHeikal <hadiiheikal@gmail.com>
2026-07-28 13:40:54 +0300

    add examples of interfaces with Vehicle and Person classes, demonstrating implementation and method definitions

---
+++ oop_revision/abstraction/mixins/mixins.dart
@@ -0,0 +1,27 @@
+// Mixins are a way of reusing the code in multiple classes.
+// Mixins are declared using the keyword mixin followed by the mixin name.
+// Three keywords are used while working with mixins: mixin, with, and on.
+// It is possible to use multiple mixins in a class.
+
+mixin ElectricVariant {
+  void electricVariant() {
+    print('This is an electric variant');
+  }
+}
+
+mixin PetrolVariant {
+  void petrolVariant() {
+    print('This is a petrol variant');
+  }
+}
+
+// with is used to apply the mixin to the class
+class Car with ElectricVariant, PetrolVariant {
+  // here we have access of electricVariant() and petrolVariant() methods
+}
+
+void main() {
+  var car = Car();
+  car.electricVariant();
+  car.petrolVariant();
+}
